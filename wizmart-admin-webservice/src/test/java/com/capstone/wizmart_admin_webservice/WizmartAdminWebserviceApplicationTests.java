7632afe4d1f3c3f44719a4d4997eea5a7c417023
ChenYJ1016 <41606624+ChenYJ1016@users.noreply.github.com>
2024-06-19 14:42:42 +0800

    Initial commit

---
+++ wizmart-admin-webservice/src/test/java/com/capstone/wizmart_admin_webservice/WizmartAdminWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.wizmart_admin_webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizmartAdminWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
